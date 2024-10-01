5109569b644c448d6409e7260302763b07395526
Shunyu Ye <145466888+Hypn0tised-ysy@users.noreply.github.com>
2024-10-01 20:19:36 +0800

    reverse face

--- Project1/fragment.fs
+++ Project1/fragment.fs
@@ -7,5 +7,5 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 void main()
 {
-FragColor=mix(texture(texture1,texture_coord)*vec4(color_output,1.0f),texture(texture2,texture_coord),0.3);
+FragColor=mix(texture(texture1,texture_coord)*vec4(color_output,1.0f),texture(texture2,vec2(1.0f-texture_coord.x,texture_coord.y)),0.3);
 }
